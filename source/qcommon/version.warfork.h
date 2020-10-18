fde75c96f197c53f810c6ef76e1a0d2946665efd
Warfork <51063252+Warfork@users.noreply.github.com>
2020-10-17 22:44:18 -0400

    change basepath

--- source/qcommon/version.warfork.h
+++ source/qcommon/version.warfork.h
@@ -105,7 +105,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 #ifndef APP_UI_BASEPATH
-#define APP_UI_BASEPATH                 "ui/porkui"
+#define APP_UI_BASEPATH                 "ui/forkui"
 #endif
 
 #ifndef APP_STARTUP_COLOR
